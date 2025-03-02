667d81bb75849c4d090b944a2912d5cc194e223a
Marko Tsymbaluk <markotsymbaluk@gmail.com>
2025-03-01 21:50:15 -0500

    ADD: the Google button now works properly

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const googleClientId =
+  "8800310716-hb9ob9ppdn9pj1ju3ecf8q5h6ffc0ptc.apps.googleusercontent.com"; // TODO: Put this in an environment variable
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <GoogleOAuthProvider clientId={googleClientId}>
+      <App />
+    </GoogleOAuthProvider>
+  </StrictMode>
+);
